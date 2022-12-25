94f3ae7d5d689ecb7308e06df11700b762d6e3b7
Ruslan <kirito7379@gmail.com>
2022-12-25 23:56:03 +0300

    Задача 19

--- Program.cs
+++ Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿Console.Write($"Введите число: ");
+string number = Console.ReadLine()!;
+if(number[0] == number[4] || number[1] == number[3])
+{
+    Console.Write($"Число {number} - Палиндроп\n");
+}
+else
+Console.Write($"Число {number} - Не Палиндроп\n");
+
+
+
+
+
+    
