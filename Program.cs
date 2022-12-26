4a715d5ada4616ea9770a39cce2ca37a65e0b837
Ruslan <kirito7379@gmail.com>
2022-12-26 19:33:45 +0300

    Задача 21

--- Program.cs
+++ Program.cs
@@ -1,14 +1,21 @@
-﻿Console.Write($"Введите число: ");
-string number = Console.ReadLine()!;
-if(number[0] == number[4] || number[1] == number[3])
-{
-    Console.Write($"Число {number} - Палиндроп\n");
-}
-else
-Console.Write($"Число {number} - Не Палиндроп\n");
+﻿int xa, xb, ya, yb, za, zb;
+Console.Write($"Введите кординату x точки a: ");
+int.TryParse(Console.ReadLine()!, out xa);  
+Console.Write($"Введите кординату y точки a: ");
+int.TryParse(Console.ReadLine()!, out ya);  
+Console.Write($"Введите кординату z точки a: ");
+int.TryParse(Console.ReadLine()!, out za);  
+Console.Write($"Введите кординату x точки b: ");
+int.TryParse(Console.ReadLine()!, out xb);  
+Console.Write($"Введите кординату y точки b: ");
+int.TryParse(Console.ReadLine()!, out yb);  
+Console.Write($"Введите кординату z точки b: ");
+int.TryParse(Console.ReadLine()!, out zb);  
 
-
-
-
+int x = (xb - xa) * (xb - xa);
+int y = (yb - ya) * (yb - ya);
+int z =(zb - za) * (zb - za);
+double ans = Math.Sqrt( x + y + z);
+Console.Write($"Кординаты точек a и b = {ans}");
 
     
